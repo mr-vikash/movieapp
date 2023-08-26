107b99146daab8464f38f5267751db9e4d2f3bdf
mr-vikash <mrvikash700@gmail.com>
2023-08-27 00:07:53 +0530

    movieapp

---
+++ src/components/Movie.jsx
@@ -0,0 +1,13 @@
+
+
+const Movie = ({title,year,img}) => {
+  return (
+    <div className="movie">
+      <img src={img} alt=""/>
+      <p>{title}</p>
+      <p>Year:{year}</p>
+    </div>
+  )
+}
+
+export default Movie
